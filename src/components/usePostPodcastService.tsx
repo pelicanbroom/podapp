dfc1399f822919477da9f086255443768bd293b3
Fabien <fccalvez@gmail.com>
2020-12-27 19:48:35 +0100

    remove timeout

--- src/components/usePostPodcastService.tsx
+++ src/components/usePostPodcastService.tsx
@@ -14,23 +14,21 @@ const usePostPodcastService = (searchTerm: string): Service<Podcasts> => {
   useEffect(() => {
     if (searchTerm) {
       setResult({ status: 'loading' });
-      setTimeout(() => {
-        fetch(
-          'https://itunes.apple.com/search?term=' +
-            searchTerm +
-            '&media=podcast'
-        )
-          .then((response) => response.json())
-          .then((response) => {
-            if (response.resultCount < 1) {
-              setResult({ status: 'init', default: 'There is no result. Try another search term' });
-            }
-            else {
-              setResult({ status: 'loaded', payload: response });
-            }
-          })
-          .catch((error) => setResult({ status: 'error', error }));
-      }, 500);
+      fetch(
+        'https://itunes.apple.com/search?term=' + searchTerm + '&media=podcast'
+      )
+        .then((response) => response.json())
+        .then((response) => {
+          if (response.resultCount < 1) {
+            setResult({
+              status: 'init',
+              default: 'There is no result. Try another search term',
+            });
+          } else {
+            setResult({ status: 'loaded', payload: response });
+          }
+        })
+        .catch((error) => setResult({ status: 'error', error }));
     } else {
       setResult({
         status: 'init',
